1fb76e9a2584f3acc70d281bef6806d1739c6f70
IsaqueTADS <isaqueofc18@gmail.com>
2026-06-18 15:52:48 -0300

    feat: add Biome configuration files and update TypeScript settings for improved development experience

--- src/server.ts
+++ src/server.ts
@@ -1,42 +1,37 @@
-import express from "express"
+import express from "express";
 import "dotenv/config";
 import { prisma } from "./lib/prisma.js";
 
+const app = express();
+const port = process.env.PORT;
 
-const app = express()
-const port = process.env.PORT 
- 
 interface User {
-  name: string
-  email: string
-  password: string
-  createdAt: Date
+	name: string;
+	email: string;
+	password: string;
+	createdAt: Date;
 }
 
-app.get('/', (req, res) => {
-  res.send('Hello World! Se inscreva no meu canal e deixa o like')
-})
+app.get("/", (req, res) => {
+	res.send("Hello World! Se inscreva no meu canal e deixa o like");
+});
 
 app.get("/users", async (req, res) => {
+	try {
+		const users = await prisma.user.findMany();
 
-  try {
-    const users =  await prisma.user.findMany()
-
-  return res.status(200).send ({users})
-  } catch (err){
-    console.error(err)
-  }
-
-  
-})
+		return res.status(200).send({ users });
+	} catch (err) {
+		console.error(err);
+	}
+});
 
 app.post("/user", (req, res) => {
-  const {name, email} = req.body as  User
+	const { name, email } = req.body as User;
 
-
-  return {name, email} as User
-})
+	return { name, email } as User;
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+	console.log(`Example app listening on port ${port}`);
+});
