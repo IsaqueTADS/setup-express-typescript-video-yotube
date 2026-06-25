ff9771ab9ce04c465962cee4ba00aa94dbd0b80a
IsaqueTADS <isaqueofc18@gmail.com>
2026-06-25 16:53:20 -0300

    feat: simplify Prisma adapter configuration by using DATABASE_URL

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -2,15 +2,9 @@ import "dotenv/config";
 import { PrismaMariaDb } from "@prisma/adapter-mariadb";
 import { PrismaClient } from "../generated/prisma/client.js";
 
-console.log(process.env.DATABASE_URL)
+console.log(process.env.DATABASE_URL);
 
-const adapter = new PrismaMariaDb({
-  host: process.env.DATABASE_HOST!,
-  port: Number(process.env.DATABASE_PORT),
-  user: process.env.DATABASE_USER!,
-  password: process.env.DATABASE_PASSWORD!,
-  database: process.env.DATABASE_NAME!,
-});
+const adapter = new PrismaMariaDb(process.env.DATABASE_URL || "");
 const prisma = new PrismaClient({ adapter });
 
 export { prisma };
